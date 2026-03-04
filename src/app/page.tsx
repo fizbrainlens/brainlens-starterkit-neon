0fa07ff248464092ac7683f2ff2cb19694f03ae6
Brainlens <hfiz.ham@gmail.com>
2026-03-04 13:46:25 +0700

    update lib and logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import Image from "next/image";
+
 export default function Home() {
-	return <div>Brainlens Starter Kit with Neon DB</div>;
+	return (
+		<div className="min-h-screen min-w-screen flex flex-col items-center justify-center">
+			<Image src="/logo.png" alt="Brainlens Logo" width={300} height={100} />
+			<h1>Starter Kit Development</h1>
+		</div>
+	);
 }
